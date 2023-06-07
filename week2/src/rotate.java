89398ff685a8964b359f3ee5ae1b655d337dd308
saikirank033 <saikirank033@gmail.com>
2023-06-07 16:10:08 -0500

    test

--- week2/src/rotate.java
+++ week2/src/rotate.java
@@ -1,22 +1,21 @@
 public class rotate {
-    public void rotate(int[] nums, int k) {
-        k %= nums.length;
+  public static void main(String args[]){
+      int[] a= {1, 2, 3, 4, 5, 6, 7};
+      int k=2;
+      int[] c=new int[a.length];
+      int j=0;
+      for(int i=a.length-k;i<a.length;i++){
 
+          c[j]=a[i];
+          System.out.println(c[j]);
+          j++;
 
-        reverse(nums, 0, nums.length - 1);
+      }
+      for (int i=0;i<a.length-k;i++){
+          c[j]=a[i];
+          System.out.println(c[j]);
+          j++;
+      }
 
-        reverse(nums, 0, k - 1);
-
-        reverse(nums, k, nums.length - 1);
-    }
-
-    public void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        }
-    }
+  }
 }
