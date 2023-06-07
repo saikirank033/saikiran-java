997141ebf94fab47f4f8b2dd9b847e2dad3cb112
saikirank033 <saikirank033@gmail.com>
2023-06-07 15:15:20 -0500

    test

---
+++ week2/src/Reverseanarray.java
@@ -0,0 +1,27 @@
+
+public class Reverseanarray {
+
+        public static int[] reverseArray(int[] array) {
+
+            int j = array.length-1;
+
+            for(int i=0; i<=j; i++)
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                j--;
+            }
+
+            return array;
+        }
+
+        public static void main(String []args){
+
+            int[] data = {1,2,3,4,5,6,7,8,9};
+            int[] a = reverseArray(data);
+            for(int i =0 ; i<a.length;i++)
+                System.out.print(a[i]+" ");
+
+        }
+    }
---
+++ week2/src/largestandsmallestnumber.java
@@ -0,0 +1,18 @@
+public class largestandsmallestnumber {
+    public static void main(String[] args) {
+
+        int numbers[] = new int[] {5,22,56,561,54,85,75,95,35,24};
+                int smallest = numbers[0];
+        int largest = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > largest)
+                largest = numbers[i];
+            else if (numbers[i] < smallest)
+                smallest = numbers[i];
+        }
+
+        System.out.println("Largest Number is : " + largest);
+        System.out.println("Smallest Number is : " + smallest);
+    }
+
+}
