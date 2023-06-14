cad074f16265012c15c3efdbcfa72e51fb6c0592
saikirank033 <saikirank033@gmail.com>
2023-06-14 11:49:07 -0500

    test

---
+++ week3/src/runtime.java
@@ -0,0 +1,20 @@
+class CE1 extends RuntimeException{
+    CE1(String s){
+        super(s);
+    }
+}
+public class runtime {
+    public static void main(String[] args) {
+        int x;
+        try{
+            x= 10/0;
+            System.out.println(x);
+        }catch(RuntimeException e){
+            throw new CE1("This is our customized Exception");
+        }
+
+    }
+
+
+
+}
